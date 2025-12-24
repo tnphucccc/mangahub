14ca78af90300bfb978f0814002f5042972af5d6
tnphucccc <tnphuc.personal@gmail.com>
2025-12-24 20:47:10 +0700

    add image configuration to next.config for remote patterns

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploads.mangadex.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
