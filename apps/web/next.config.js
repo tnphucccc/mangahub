a33e1e1bf4a4f809abd4418cdec38f52cbd47d3d
Khang Huy <khatihuy125@gmail.com>
2025-12-24 18:20:23 +0700

    add host

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['uploads.mangadex.org', 'via.placeholder.com', 'example.com'],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
